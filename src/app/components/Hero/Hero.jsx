542dd92078fd77a21b641ebdcc1bcc975b6741cf
Ayegba John <ayegbajohn@gmail.com>
2025-03-23 23:51:57 +0100

    minor h1

---
+++ src/app/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className={styles.Hero}>
+        <div>
+          <h1>Boost Your Career with Accredited Master of Science Degree</h1>
+        </div>
+        <p>
+          Our 100% online Master's programs enable you to obtain internationally
+          recognized higher education degrees in tech.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
