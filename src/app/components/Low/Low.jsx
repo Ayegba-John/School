3b5368de0680f523d56173d03fe06da65f31bd5a
Ayegba John <ayegbajohn@gmail.com>
2025-04-02 23:42:22 +0100

    minor

---
+++ src/app/components/Low/Low.jsx
@@ -0,0 +1,13 @@
+import styles from "./Low.module.css";
+const low = () => {
+  return (
+    <div>
+      <div className={styles.Container}>
+        <div>item 1</div>
+        <div>item 2 </div>
+        <div>item 3</div>
+      </div>
+    </div>
+  );
+};
+export default low;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,14 @@ import styles from "./page.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Body from "./components/Body/Body";
-
+import Low from "./components/Low/Low";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Body />
+      <Low />
     </div>
   );
 };
