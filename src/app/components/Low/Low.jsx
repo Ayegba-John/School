992ee1488216281a4df2707c67aea9fa4e5bd07b
Ayegba John <ayegbajohn@gmail.com>
2025-04-03 22:22:35 +0100

    feat

--- src/app/components/Low/Low.jsx
+++ src/app/components/Low/Low.jsx
@@ -3,9 +3,28 @@ const low = () => {
   return (
     <div>
       <div className={styles.Container}>
-        <div>item 1</div>
-        <div>item 2 </div>
-        <div>item 3</div>
+        <div>
+          <p>
+            English proficiency of at least B2 level Demonstrated by a
+            certificate or proof of previous undergraduate study in English
+          </p>
+        </div>
+        <div>
+          <p>
+            Up-to-date computer A computer on which you can freely install tools
+            and which has an actively supported and updated operating system
+          </p>
+        </div>
+        <div>
+          Dedication of 15-30 focused hours per week Focused hours are:  -
+          Working with the computer (not a smartphone)  - Undisrupted working
+        </div>
+        <div>
+          <p>
+            Bachelor’s degree or license involving technical skills or 5 years
+            of work experience + 2-year qualification or Other options available
+          </p>
+        </div>
       </div>
     </div>
   );
