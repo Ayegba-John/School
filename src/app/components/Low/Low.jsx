f8a7413784cd51e708eac2e0fb53942df0f1df20
Ayegba John <ayegbajohn@gmail.com>
2025-04-06 23:42:38 +0100

    changes

--- src/app/components/Low/Low.jsx
+++ src/app/components/Low/Low.jsx
@@ -1,17 +1,18 @@
 import styles from "./Low.module.css";
+
 const low = () => {
   return (
     <div>
       <div className={styles.Container}>
         <div>
-          <img src="Group-166.svg" alt="Group" />
+          <img src="Group-166.svg" alt="" />
           <p>
             English proficiency of at least B2 level Demonstrated by a
             certificate or proof of previous undergraduate study in English
           </p>
         </div>
         <div>
-          <img src="Group-up.svg" alt="group" />
+          <img src="Group-b.svg" alt="" />
           <p>
             Up-to-date computer A computer on which you can freely install tools
             and which has an actively supported and updated operating system
@@ -20,16 +21,18 @@ const low = () => {
       </div>
       <div className={styles.Container}>
         <div>
+          <img src="Group-d.svg" alt="" />
           <p>
-            <img src="Group-d.svg" alt="" />
-            Dedication of 15-30 focused hours per week Focused hours are:  -
-            Working with the computer (not a smartphone)  - Undisrupted working
+            Dedication of 15-30 focused hours per week Focused hours are:
+            Working with the computer (not a smartphone) Undisrupted working
           </p>
         </div>
+
         <div>
+          <img src="Group-d.svg" alt="" />
           <p>
-            Bachelor’s degree or license involving technical skills or 5 years
-            of work experience + 2-year qualification or Other options available
+            Bachelors degree or license involving technical skills or 5 years of
+            work experience + 2-year qualification or Other options available
           </p>
         </div>
       </div>
