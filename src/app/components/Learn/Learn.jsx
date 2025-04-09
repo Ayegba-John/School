61e5f405e0baa936e96f12dec13548c0fbdac723
Ayegba John <ayegbajohn@gmail.com>
2025-04-09 21:44:47 +0100

    feat:minor

--- src/app/components/Learn/Learn.jsx
+++ src/app/components/Learn/Learn.jsx
@@ -1,10 +1,37 @@
 import styles from "./Learn.module.css";
+
 const learn = () => {
   return (
     <div>
       <div className={styles.main}>
-        <div> right</div>
-        <div>left </div>
+        <div className={styles.left}>
+          <div>
+            <img className={styles.icon} src="groupe-r.svg" alt="icon" />
+          </div>
+
+          <h2>Learn with highly qualified instructors</h2>
+          <p>
+            In addition to being experts in their fields, they are mentors at
+            heart and focus on each student’s needs.
+          </p>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.right_up}>
+            <img
+              className={styles.image}
+              src="Rectangle-57.jpg"
+              alt="Group pic"
+            />
+          </div>
+          <div className={styles.right_down}>
+            <div>
+              <img src="Rectangle-59-1.png" alt="Group" />
+            </div>
+            <div>
+              <img src="Rectangle-60-1.png" alt="Group" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
