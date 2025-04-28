351007f5859ec5fd8b1a8bf8017e47dcc9d92125
Ayegba John <ayegbajohn@gmail.com>
2025-04-28 23:28:39 +0100

    feat:staff

--- src/app/components/Testimony/Testimony.jsx
+++ src/app/components/Testimony/Testimony.jsx
@@ -15,6 +15,73 @@ const test = () => {
           <img className={styles.picture} src="testimo.jpg" alt="" />
         </div>
       </div>
+      <div>
+        <h2>Dean</h2>
+        <div className={styles.test}>
+          <img src="Osuma-88.svg" alt="pic of dean" />
+          <p>
+            As Chief Product Officer at GOMYCODE, I lead teams to provide
+            affordable digital skills training, reaching over 30,000 students in
+            40 cities across 10 countries. I focus on creating impactful
+            products for underserved users, applying scientific and
+            technological knowledge for social impact. With master’s degrees in
+            electrical engineering and data science from École Polytechnique and
+            MINES ParisTech, my career spans diverse environments. A quick
+            learner, I tackle operational, technical, and strategic challenges
+            with ongoing curiosity.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Academic board members</h2>
+        <div className={styles.test}>
+          <img src="Osuma-88.svg" alt="pic of dean" />
+          <p>
+            Kaïs Mabrouk, a graduate of the National School of
+            Telecommunications and Sciences Po Paris Executive Education, has
+            extensive experience in higher education management. He has worked
+            at SAFRAN, E-Blink, and ESIGETEL-EFREI, where he led international
+            expansion. Dr. Mabrouk founded The International Institute of
+            Digital and Audiovisual (IINA) and is now Executive Director of
+            International Development at Université Central group. He also
+            teaches at Saint Petersburg Electrotechnical University "LETI".
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Dean</h2>
+        <div className={styles.test}>
+          <img src="Osuma-88.svg" alt="pic of dean" />
+          <p>
+            As Chief Product Officer at GOMYCODE, I lead teams to provide
+            affordable digital skills training, reaching over 30,000 students in
+            40 cities across 10 countries. I focus on creating impactful
+            products for underserved users, applying scientific and
+            technological knowledge for social impact. With master’s degrees in
+            electrical engineering and data science from École Polytechnique and
+            MINES ParisTech, my career spans diverse environments. A quick
+            learner, I tackle operational, technical, and strategic challenges
+            with ongoing curiosity.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Dean</h2>
+        <div className={styles.test}>
+          <img src="Osuma-88.svg" alt="pic of dean" />
+          <p>
+            As Chief Product Officer at GOMYCODE, I lead teams to provide
+            affordable digital skills training, reaching over 30,000 students in
+            40 cities across 10 countries. I focus on creating impactful
+            products for underserved users, applying scientific and
+            technological knowledge for social impact. With master’s degrees in
+            electrical engineering and data science from École Polytechnique and
+            MINES ParisTech, my career spans diverse environments. A quick
+            learner, I tackle operational, technical, and strategic challenges
+            with ongoing curiosity.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
