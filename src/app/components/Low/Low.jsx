ba4ee2c248a00bc7e5275de2564aca153444a4a2
Ayegba John <ayegbajohn@gmail.com>
2025-07-03 23:48:57 +0100

    minor

--- src/app/components/Low/Low.jsx
+++ src/app/components/Low/Low.jsx
@@ -14,10 +14,6 @@ const low = () => {
             English proficiency of at least B2 level Demonstrated by a
             certificate or proof of previous undergraduate study in English
           </p>
-          <p>
-            English proficiency of at least B2 level Demonstrated by a
-            certificate or proof of previous undergraduate study in English
-          </p>
         </div>
         <div>
           <img src="Group-b.svg" alt="" />
