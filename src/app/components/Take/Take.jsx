d65b37f2e8916bdc16c703432d7ca8c0532b5f3c
Ayegba John <ayegbajohn@gmail.com>
2025-07-19 23:59:31 +0100

    sample

---
+++ src/app/components/Take/Take.jsx
@@ -0,0 +1,12 @@
+import styles from ".Take.module.css";
+
+const take = () => {
+  return (
+    <div>
+      <p className={styles.main}>hello</p>
+      <p className={styles.main}>hello</p>
+      <p className={styles.main}>hello</p>
+    </div>
+  );
+};
+export default take;
